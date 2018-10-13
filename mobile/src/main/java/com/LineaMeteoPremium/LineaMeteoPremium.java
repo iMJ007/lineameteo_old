998c8f64a35ceebadfb30b1eb5b83c7a4cd59a6c
Steve Hartken <steve.hartken@gmail.com>
2018-10-12 21:24:47 -0400

    Working app widget update -- Possible caching
    https://stackoverflow.com/a/52779460/586631

--- mobile/src/main/java/com/LineaMeteoPremium/LineaMeteoPremium.java
+++ mobile/src/main/java/com/LineaMeteoPremium/LineaMeteoPremium.java
@@ -1,18 +1,21 @@
 package com.LineaMeteoPremium;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.transition.Transition;
 import com.robotemplates.webviewapp.R;
 import com.bumptech.glide.request.target.AppWidgetTarget;
 
@@ -23,6 +26,7 @@ public class LineaMeteoPremium extends AppWidgetProvider {
 
     private AppWidgetTarget appWidgetTarget;
     public String src="http://retemeteo.lineameteo.it/banner/big.php?ID=1";
+    public Boolean srcDefault = false;
 
     static void updateAppWidget(Context context, RemoteViews remoteViews, int appWidgetId) {
 
@@ -35,32 +39,36 @@ public class LineaMeteoPremium extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.linea_meteo_premium);
+        for (int widgetId : appWidgetIds) {
+            setUpAlarm(context, widgetId);
 
-        AppWidgetTarget awt = new AppWidgetTarget(context, R.id.imageView, remoteViews, appWidgetIds) {
-            @Override
-            public void onResourceReady(Bitmap resource, Transition<? super Bitmap> transition) {
-                super.onResourceReady(resource, transition);
-            }
-        };
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.linea_meteo_premium);
 
-        RequestOptions options = new RequestOptions().
-                override(300, 300).placeholder(R.drawable.navigation_header_bg).error(R.drawable.ic_navigation_help);
+            Log.e("LineaMeteo Widget", "src " + src);
+
+            AppWidgetTarget awt = new AppWidgetTarget(context, R.id.imageView, remoteViews, appWidgetIds);
+
+            RequestOptions options = new RequestOptions().
+                    override(300, 300).placeholder(R.drawable.navigation_header_bg).error(R.drawable.ic_navigation_help).skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE);
 
 
-        // Create an Intent to launch Browser
-        Intent intent = new Intent(context, WidgetConfig.class);
+            // Create an Intent to launch Browser
+            Intent intent = new Intent(context, WidgetConfig.class);
 
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(context, 0, intent, 0);
+            PendingIntent pendingIntent =
+                    PendingIntent.getActivity(context, 0, intent, 0);
 
-        Log.i("LineaMeteo", "src " + src);
-        remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
-        Glide.with(context.getApplicationContext())
-                .asBitmap()
-                .load(src)
-                .apply(options)
-                .into(awt);
+
+            remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+
+            Glide.with(context.getApplicationContext())
+                    .asBitmap()
+                    .load(src)
+                    .apply(options)
+                    .into(awt);
+        }
+
+
 
     }
 
@@ -73,6 +81,57 @@ public class LineaMeteoPremium extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        switch (intent.getAction()) {
+            case AppWidgetManager.ACTION_APPWIDGET_UPDATE:
+                onUpdate(context, AppWidgetManager.getInstance(context), AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, LineaMeteoPremium.class)));
+                break;
+            default:
+                super.onReceive(context, intent);
+        }
+    }
+
+    /**
+     * Sets up widget refresh alarm
+     *
+     * @param context     The context of widget
+     * @param appWidgetId Widget ID of current widget
+     */
+    private void setUpAlarm(Context context, int appWidgetId) {
+        final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        long interval = (3 * 60000); // 60000 = 1minute  | Change the formula based on your refresh timing needs
+
+        PendingIntent alarmPendingIntent = getRefreshWidgetPendingIntent(context, appWidgetId);
+
+        alarm.cancel(alarmPendingIntent);
+
+        // SET NEW ALARM
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarm.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + interval, alarmPendingIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarm.setExact(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + interval, alarmPendingIntent);
+        } else {
+            alarm.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + interval, alarmPendingIntent);
+        }
+    }
+
+    /**
+     * Get the pending intent object for refreshing the widget
+     *
+     * @param context  current Context
+     * @param widgetId - Current Widget ID
+     * @return - Pending intent for refreshing widget
+     */
+    private PendingIntent getRefreshWidgetPendingIntent(Context context, int widgetId) {
+        Intent intent = new Intent(context, LineaMeteoPremium.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+        // Make the pending intent unique...
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        return PendingIntent.getBroadcast(context, 123, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 }
 
